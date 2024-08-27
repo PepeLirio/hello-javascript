85205e9d21d39bff6e87716e024b09989b5d17d8
PepeLirio <pepeliriogil@gmail.com>
2024-08-27 17:53:12 +0200

    conceptos básicos

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -2,3 +2,22 @@ console.log("¡Hola JavaScript!"); //Comillas dobles
 console.log('¡Hola JavaScript!'); //Comillas simples
 
 //El ; no es obligatorio en JavaScript
+
+console.log(""); //Esto lo hago como salto de línea.
+
+/* Aclarar que esto es
+un comentario 
+en varias líneas */
+
+/* Los número se pueden representar como cadena de textos con ""
+o como número, simplemente poniendo el número */
+console.log("5");
+console.log(5);
+
+console.log("");
+
+console.log("Operaciones básicas: ");
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
